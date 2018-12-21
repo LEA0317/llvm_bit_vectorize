378080c3904a40aef104dca2479bfbea926b751b
konda <konda@leapmind.io>
2018-12-21 18:32:30 +0900

    modify test

--- test/test_ng.cpp
+++ test/test_ng.cpp
@@ -1,10 +1,10 @@
 typedef bool v32i1 __attribute__((ext_vector_type(32)));
 
 // wanna compile (should bitconvert, but no way)
-unsigned int test2(bool **b) {
+unsigned int test(bool *b) {
   v32i1 a;
   for (int i=0; i<32; i++) {
-    a[i] = *b[i];
+    a[i] = b[i];
   }
   return a;
 }
