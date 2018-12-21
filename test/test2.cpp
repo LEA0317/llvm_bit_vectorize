cdff93302088cced394d98a037ecd614e0a8aa9c
konda <konda@leapmind.io>
2018-12-21 16:48:52 +0900

    modify test

--- test/test2.cpp
+++ test/test2.cpp
@@ -1,9 +1,9 @@
 typedef bool v32i1 __attribute__((ext_vector_type(32)));
 
-v32i1 test(bool **b) {
+v32i1 test(bool *b) {
   v32i1 a;
   for (int i=0; i<32; i++) {
-    a[i] = *b[i];
+    a[i] = b[i];
   }
   return a;
 }
---
+++ test/test3.cpp
@@ -0,0 +1,30 @@
+typedef bool v32i1 __attribute__((ext_vector_type(32)));
+
+v32i1 test(bool *b) {
+  v32i1 a;
+  for (int i=0; i<32; i++) {
+    a[i] = b[i];
+  }
+  return a;
+}
+
+v32i1 mult(v32i1 a, v32i1 b) {
+  return a * b;
+}
+
+int main(void) {
+  bool array1[] = {0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,1,1,0,1};
+  bool array2[] = {0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,0,0,
+		   0,0,0,0,0,0,1,1};
+  v32i1 a = test(array1);
+  v32i1 b = test(array2);  
+
+  v32i1 ret = mult(a, b); // cannot convert return value for now
+
+  return 0;
+}
